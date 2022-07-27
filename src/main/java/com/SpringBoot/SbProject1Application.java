cb8ec9cd5a8d0cbc5f4a1d1cdd9d94874d3cdcc4
suresh <srajrec95@gmail.com>
2022-07-27 18:10:01 +0530

    second commit

--- src/main/java/com/SpringBoot/SbProject1Application.java
+++ src/main/java/com/SpringBoot/SbProject1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SbProject1Application {
 
 	int a=10;
+	int b=20;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbProject1Application.class, args);
